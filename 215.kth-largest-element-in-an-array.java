a8c598b08e71ca7f029d07b37a36b7554ae7ea0e
Nishita2022 <123723082+Nishita2022@users.noreply.github.com>
2023-02-02 23:11:30 +0530

    Added question 215 of Leetcode

---
+++ 215.kth-largest-element-in-an-array.java
@@ -0,0 +1,30 @@
+import java.util.PriorityQueue;
+
+/*
+ * @lc app=leetcode id=215 lang=java
+ *
+ * [215] Kth Largest Element in an Array
+ */
+
+// @lc code=start
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i=0; i<k; i++){
+            pq.add(nums[i]);
+        }
+        
+         for(int i=k; i< nums.length ; i++){
+            if (pq.peek() < nums[i]){
+                pq.poll();
+                pq.add(nums[i]);
+            }
+         }
+        return pq.peek();
+
+    
+}
+
+}
+// @lc code=end
+
